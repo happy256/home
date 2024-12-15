aa694b36fdc8c74373b321bddda9f367c93eae68
happy256 <54813464+happy256@users.noreply.github.com>
2024-12-15 16:04:29 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,7 @@
             -
           </span>
           {{ fullYear }}
-          <a :href=#>{{ siteAuthor }}</a>
+          <a :href="siteUrl">{{ siteAuthor }}</a>
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">

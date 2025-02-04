148f735d3928b53d2e7acbedec83ac2893174adf
happy256 <54813464+happy256@users.noreply.github.com>
2025-02-04 18:13:43 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Video,Audible,PhotoVideo,NetworkWired,Npm,Ethernet} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Video,Audible,PhotoVideo,NetworkWired,Npm,Ethernet,Brain} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -76,6 +76,7 @@ const siteIcon = {
   NetworkWired,
   Npm,
   Ethernet,
+  Brain
 };
 
 // 链接跳转
